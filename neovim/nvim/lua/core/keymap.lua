8f5898c89b9e915ae1a40b120d9b17f53fcafa97
am-shm <am.shahmoradi93@gmail.com>
2024-08-27 14:33:52 +0330

    toggle term style changed to open terminal horizontally splited

--- neovim/nvim/lua/core/keymap.lua
+++ neovim/nvim/lua/core/keymap.lua
@@ -17,7 +17,12 @@ vim.opt.expandtab = true
 --Line numbers
 vim.wo.number = true
 
---custom kemaps
-vim.keymap.set('n','<C-t>',':ToggleTerm direction=float<CR>')
+-- Custom kemaps:
+
+-- toggle term -> ctrl + t openning terminal / ctrl + shift + t toggle terminal
+vim.keymap.set('n','<C-t>',':ToggleTerm direction=horizontal size=10<CR>')
+vim.keymap.set('n','<C-S-t>',':ToggleTerm<CR>')
+
+-- definition and declaration -> gd definition / gD declaration
 vim.api.nvim_set_keymap('n',"gD","<cmd>lua vim.lsp.buf.declaration()<CR>", {noremap = true , silent = true} )
 vim.api.nvim_set_keymap('n',"gd","<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true , silent = true} )
